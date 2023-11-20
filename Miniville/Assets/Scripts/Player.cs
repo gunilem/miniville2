083c48dc62b23a36365a47b54b451422e5566094
Andolink <andoni.lapeyre@gmail.com>
2023-11-20 09:29:50 +0100

    hjk

---
+++ Miniville/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Player : MonoBehaviour
+{
+    public int nCoins { get; private set; }
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
